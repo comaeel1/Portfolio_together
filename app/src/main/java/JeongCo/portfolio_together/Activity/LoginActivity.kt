eb843415c649465edc69ab249e092d3d23194338
corea95 <50865732+corea95@users.noreply.github.com>
2021-03-15 20:46:20 +0900

    로그인 및 회원가입 진행중

--- app/src/main/java/JeongCo/portfolio_together/Activity/LoginActivity.kt
+++ app/src/main/java/JeongCo/portfolio_together/Activity/LoginActivity.kt
@@ -1,12 +1,54 @@
 package JeongCo.portfolio_together.Activity
 
-import JeongCo.portfolio_together.R
-import androidx.appcompat.app.AppCompatActivity
+import JeongCo.portfolio_together.Retrofit.ServiceApi
+import JeongCo.portfolio_together.databinding.ActivityLoginBinding
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LoginActivity : AppCompatActivity() {
+    val binding by lazy {ActivityLoginBinding.inflate(layoutInflater)}
+    var compositeDisposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val service: ServiceApi =
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+        }
     }
+
+    fun attemptLogin(){
+        var userID = binding.tvUserid.text.toString()
+        var userPwd = binding.tvPwd.text.toString()
+        var cancel = false
+
+        if(userID.isEmpty()) {
+            Toast.makeText(this@LoginActivity, "아이디를 입력해주세요!", Toast.LENGTH_SHORT).show()
+            cancel = true
+        }else if(!isUseridValid(userID)) {
+            Toast.makeText(this@LoginActivity, "아이디는 최소 4글자 이상입니다", Toast.LENGTH_SHORT).show()
+            cancel = true
+        }
+
+        if(userPwd.isEmpty()) {
+            Toast.makeText(this@LoginActivity, "비밀번호를 입력해주세요!", Toast.LENGTH_SHORT).show()
+            cancel = true
+        }else if(!isUserPwdValid(userPwd)) {
+            Toast.makeText(this@LoginActivity, "비밀번호는 최소 6글자 이상입니다", Toast.LENGTH_SHORT).show()
+            cancel = true
+        }
+
+        if(!cancel){
+
+        }
+
+    }
+
+    fun StartLogin(){
+
+    }
+    fun isUseridValid(Userid: String): Boolean = Userid.length >= 4
+    fun isUserPwdValid(userPwd: String): Boolean = userPwd.length > 6
 }
---
+++ app/src/main/java/JeongCo/portfolio_together/Retrofit/INodeJS.kt
@@ -0,0 +1,12 @@
+package JeongCo.portfolio_together.Retrofit
+
+import retrofit2.http.POST
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+
+interface INodeJS {
+    /*@POST("/user/login")
+    @FormUrlEncoded
+    fun registerUser(@Field(""))*/
+
+}
